1f64dde83417b128c1e8cfcaed0534e495de5add
TurnerMooreDavis <turnermdavis@gmail.com>
2015-05-14 13:25:05 -0400

    initial commit

---
+++ employee_review_test.rb
@@ -0,0 +1,28 @@
+require 'minitest/autorun'
+require 'minitest/pride'
+
+require "./employee.rb"
+require "./department.rb"
+
+class ReviewTest < Minitest::Test
+
+  def test_classes_exist
+    assert Employee
+    assert Department
+  end
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+end
